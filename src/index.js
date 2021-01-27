6c3c0be31fb1f938877626a174a1a2c715f3e313
Vladyslav Suhyh <vcyx@ukr.net>
2021-01-27 20:18:41 +0200

    deleted webpack, added readme.md

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import Post from "./Post.js";
-import "./styles/styles.css";
-import "./styles/styles.scss";
-
-const post = new Post("Webpack Post Title");
-
-console.log("Post to string:", post.toString());
