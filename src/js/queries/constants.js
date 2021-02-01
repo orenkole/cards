073bc0f9556f7ce8023743581fbc37ca3beaafb8
Vladyslav Suhyh <vcyx@ukr.net>
2021-02-01 21:42:45 +0200

    clear branch DEV

--- src/js/queries/constants.js
+++
@@ -1,2 +0,0 @@
-export const url = "https://ajax.test-danit.com/api/cards";
-export const token = "8aa26442-4ba6-460a-af54-20a959eaf72e";
--- мусор/js/components/createVisitBtnObj.js
+++
@@ -1,5 +0,0 @@
-import Button from "../classes/Button.js";
-
-export const createVisitBtnObj = new Button()
-
-export const createVisitBtnElement = createVisitBtnObj.element;
